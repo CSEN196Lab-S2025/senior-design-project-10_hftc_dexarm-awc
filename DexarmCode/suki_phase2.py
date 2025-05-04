15614d39b7eac2b7145198820559c4cb70e93133
DavidT <104195488+Dthuita@users.noreply.github.com>
2025-05-04 01:45:37 -0700

    Create suki_phase2.py

---
+++ DexarmCode/suki_phase2.py
@@ -0,0 +1,17 @@
+import magneto_library
+import suki_library
+import time
+from pydexarm import Dexarm
+
+MAGNETO = Dexarm(port="COM4") # the com # for MAGNETO
+
+
+suki_library.dexarm_init(MAGNETO)
+
+magneto_library.small_pressure(MAGNETO)
+
+magneto_library.mini_solenoid_ON()
+
+magneto_library.Finished(MAGNETO)
+
+magneto_library.mini_solenoid_OFF()
