fa58c16a4108c40974f4e6fd69f7bd7f0ee2e479
DavidT <104195488+Dthuita@users.noreply.github.com>
2025-05-04 01:42:16 -0700

    Update magneto_phase1.py

--- DexarmCode/magneto_phase1.py
+++ DexarmCode/magneto_phase1.py
@@ -1,4 +1,4 @@
-import pydexarm_NameTag
+import magneto_library
 import pydexarm_Signage
 import time
 from pydexarm import Dexarm
@@ -9,25 +9,25 @@ MAGNETO = Dexarm(port="COM4") # the com # for MAGNETO
 pydexarm_Signage.dexarm_init(MAGNETO)
 
 # picking up the name tag from starting position ===== DONE =====
-pydexarm_NameTag.RetrievingNameTag(MAGNETO) # getting the name tag
+magneto_library.RetrievingNameTag(MAGNETO) # getting the name tag
 
-pydexarm_NameTag.PlaceToFlip(MAGNETO) # placing the name tag to the flipping mechanism
+magneto_library.PlaceToFlip(MAGNETO) # placing the name tag to the flipping mechanism
 
-pydexarm_NameTag.name_tag_flipper() # flipping the name tag
+magneto_library.name_tag_flipper() # flipping the name tag
 
-pydexarm_NameTag.MagneticPickUppart1(MAGNETO) # lining up to pick up the magnet
+magneto_library.MagneticPickUppart1(MAGNETO) # lining up to pick up the magnet
 
-pydexarm_NameTag.mini_solenoid_ON()
-pydexarm_NameTag.MagneticPickUppart2(MAGNETO) # lowering down the dexarm so that the magnet can actually be picked up
+magneto_library.mini_solenoid_ON()
+magneto_library.MagneticPickUppart2(MAGNETO) # lowering down the dexarm so that the magnet can actually be picked up
 
-pydexarm_NameTag.MagneticApplication(MAGNETO)
-pydexarm_NameTag.mini_solenoid_OFF()
+magneto_library.MagneticApplication(MAGNETO)
+magneto_library.mini_solenoid_OFF()
 
-pydexarm_NameTag.small_pressure(MAGNETO)
+magneto_library.small_pressure(MAGNETO)
 
-pydexarm_NameTag.mini_solenoid_ON()
+magneto_library.mini_solenoid_ON()
 
-pydexarm_NameTag.Finished(MAGNETO)
+magneto_library.Finished(MAGNETO)
 
-pydexarm_NameTag.mini_solenoid_OFF()
+magneto_library.mini_solenoid_OFF()
 
