e0a2f24e38307411ffe71f241f4427a355b97b4c
DavidT <104195488+Dthuita@users.noreply.github.com>
2025-05-04 02:09:09 -0700

    Update lulu_library.py
    position 2 and position init update (added 5) inorder to perfect

--- DexarmCode/lulu_library.py
+++ DexarmCode/lulu_library.py
@@ -1,282 +1,144 @@
-import sys, math, time, serial
-from pydexarm import Dexarm
+#include <Arduino.h>
+#include <Wire.h>
+#include <vl53l4cd_class.h>
+#include <AccelStepper.h>
 
-serial_connection = None
-ARDUINO_PORT = 'COM7'
-SPEED_ROBOT = 6000
-speedRobot = 6000
+// #define Serial Serial
+#define MEASUREMENTS 10 // Number of measurements to take
 
-x_init = -240
-y_init = 0
-z_init = 30 
-fast_rate = 6000
-slow_rate = 1000
+AccelStepper stepper(1, 8, 9);// direction Digital 9 (CCW), pulses (or step) Digital 8 (CLK)
+const int limitSwitch = 10;
+int position_home, position_init, position1, position2;
 
+VL53L4CD sensor_vl53l4cd_sat(&Wire, A1);
 
-class LULU_ARDUINO:
-    # important note, when setting up the dexarm make sure to have 
-    # the button to turn on the dexarm facing towards the conveyor belt
-    # then move dexarm to th
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
 
-    def __init__(self):
-        self.ser = serial.Serial(ARDUINO_PORT, baudrate=115200, timeout=1)
-        time.sleep(2)  # Allow time for the serial connection to establish
-        print("Serial connection established with Arduino for LULU", ARDUINO_PORT)
+  Serial.begin(115200);
+  while (!Serial); // Wait for Serial port to connect
+  Serial.println("Sensor Ready...");
 
-    def close_comms(self):
-        self.ser.close()
-        print("Serial connection closed with Arduino stepper motor.")
+  Wire.begin();
 
-    def stepperInit(self): # this function inits stepper w/ home then sends it to 90deg angle so dexarm has room to init
-        # Command the Arduino to initialize stepper motor
-        self.ser.write(b"$H\n")
-        time.sleep(4)
-        # Command the Arduino to move stepper to 90deg
-        self.ser.write(b"$I\n")
+  sensor_vl53l4cd_sat.begin();
+  sensor_vl53l4cd_sat.VL53L4CD_Off();
+  sensor_vl53l4cd_sat.InitSensor();
+  sensor_vl53l4cd_sat.VL53L4CD_SetRangeTiming(200, 0);
 
-    def stepperHome(self):  # this function inits home position then sets dexarm platform parallel to laser door
-        # Command the Arduino to start measurement
-        self.ser.write(b"$H\n")
+  //min MaxSpeed = 200; min Acceleration = 400
+  stepper.setMaxSpeed(200); //SPEED = Steps / second
+  stepper.setAcceleration(400); //ACCELERATION = Steps /(second)^2
+  stepper.enableOutputs(); //enable pins
 
-    def stepperP1(self):
-        # Command the Arduino to start measurement
-        self.ser.write(b"$P1\n")
+  //limit switch
+  pinMode(limitSwitch, INPUT); // Enable internal pull-up resistor
+}
+void move(int position){
+  stepper.moveTo(position); //full rotation
+  stepper.runToPosition();
+}
+void init_home() { //initialized positions on stepper motor then go to home
+  // Move left "forever" until the limit switch is hit
+  stepper.moveTo(-3200); // large negative number to go left "indefinitely"
 
-    def stepperP2(self):
-        # Command the Arduino to start measurement
-        self.ser.write(b"$P2\n")
+  while (true) {
+    if (digitalRead(limitSwitch) == HIGH) {
+      // Emergency hard stop
+      stepper.setCurrentPosition(0); // Reset position (optional)
+      stepper.stop(); // Prepare to stop
+      break;
+    }
+      stepper.run();
+  }
 
-    def get_sensor_height(self):  #Weird behavior sometimes it doesn't work
-        CALIBRATION_OFFSET = 40.0
+  delay(200); // Give motor time to settle
 
-        #clear serial communication before hand # VERY IMPORTANT
-        self.ser.flush()
-        time.sleep(5)
+  // int stepperEdge = stepper.currentPosition();
 
-        # Command the Arduino to start measurement
-        print("getting measurements...")
-        self.ser.write(b"$M\n")
-        raw_measurement = None
+  position_home = stepper.currentPosition() + 20;
+  position1 = position_home + 30;
+  position2 = position_home + 205;
+  position_init = position_home + 105;
 
-        # Wait for and process the incoming data
-        while True:
-            if self.ser.in_waiting > 0:
-                line = self.ser.readline().decode().strip()
-                print("line: ", line)
-                if line.startswith("$M"):
-                    # Parse the average distance measurement
-                    raw_measurement = float(line[2:])
-                elif line == "$D":
-                    # End of measurement data transmission
-                    break
+  Serial.print("stepper home position: ");
+  Serial.println(position_home);
 
-        if raw_measurement is None:
-            print("Measurement error or no data received.")
-            return None
+  //move to home position
+  stepper.moveTo(position_home);
+    while (stepper.distanceToGo() != 0) {
+    stepper.run();
+  }
 
-        # Apply the calibration offset and round the result
-        calibrated_measurement = round(raw_measurement - CALIBRATION_OFFSET, 1)
+  Serial.println("HOME!");
+}
+void distSensor(){
+  float totalDistance = 0;
+  int validMeasurements = 0;
 
-        # Debug message
-        print(f"Debug Info: Raw measurement = {raw_measurement} mm, "
-            f"Calibrated measurement = {calibrated_measurement} mm, "
-            f"Current offset = {CALIBRATION_OFFSET} mm")
-
-        return calibrated_measurement
-
-class Laserdoor_ARDUINO:
-    def __init__(self):
-        self.ser = serial.Serial('COM13', baudrate=115200, timeout=1)
-        time.sleep(2)  # Allow time for the serial connection to establish
-        print("Serial connection established with laser door, will OPEN DOOR now", 'COM13')
-
-    def close_comms(self):
-        self.ser.close()
-        print("Serial connection closed with Arduino stepper motor.")
-
-    def laser_door_open():
-        # Command the Arduino to open the door
-        serial_connection.write(b"$S\n")
-        print("Laser Door opened")
-
-    def laser_door_close():
-        # Command the Arduino to CLOSE the door
-        serial_connection.write(b"$K\n") #
-        print("Laser Door closed")
-
-# initializing the DexArm
-def material_initialize(dexarm):
-    dexarm.go_home()
-    dexarm.rotate_init()
-    dexarm.move_to(-140, 200, 30, mode = "G0", feedrate = fast_rate)
-    dexarm.move_to(x_init, y_init, z_init,mode = "G0", feedrate = fast_rate, wait = True) # these values are defined globally and can be found at the top
-    time.sleep(5)
-    print("========================== The dexarm for the material is initialized")
-    # print("$") # this is the function for the turn table to initialize itself
+  for (int i = 0; i < MEASUREMENTS; ++i) {
+    sensor_vl53l4cd_sat.VL53L4CD_StartRanging();
     
-# then the turn table will move ot the angle needed to pick up the material
+    uint8_t NewDataReady = 0;
+    VL53L4CD_Result_t results;
+    uint8_t status;
 
-def rotate_first(dexarm):
-    dexarm.rotate_to_position(30)
+    // Wait for new data to be ready
+    do {
+      status = sensor_vl53l4cd_sat.VL53L4CD_CheckForDataReady(&NewDataReady);
+    } while (!NewDataReady);
 
-# picking up the material with checkin of distance sensor
-def grab_blank_material(dexarm, arduino):
-    grab_x = x_init - 152
-    grab_y = y_init
-    z_max = 120 #120 # this is what they used last year
+    digitalWrite(LED_BUILTIN, HIGH); // LED on
 
-    # dexarm.move_to(-80, 250, 30, mode = "G0", feedrate = fast_rate)
-    # dexarm.move_to(-70, 320, -50, mode = "G0", feedrate = fast_rate)
+    sensor_vl53l4cd_sat.VL53L4CD_GetResult(&results);
+    sensor_vl53l4cd_sat.VL53L4CD_ClearInterrupt(); // Clear interrupt for next measurement
 
-    dexarm.move_to(grab_x, grab_y, z_init, mode = "G0", feedrate = 3000) 
-    # print(" lets see if this works * fingers crossed* ")
+    digitalWrite(LED_BUILTIN, LOW); // LED off
 
-    height = arduino.get_sensor_height()
+    if ((!status) && (NewDataReady != 0) && (results.range_status == 0)) {
+      totalDistance += results.distance_mm;
+      ++validMeasurements;
+    }
 
-    time.sleep(3)
-    print("========================== height: ", height)
-    hover_signage = z_max - height # this is meant to make sure that the dexarm doesn't go too far down
-    grab_signage = -height + 25 # this is the height to grab the material
-    
-    dexarm.air_picker_pick()
-    dexarm.move_to(grab_x, grab_y, hover_signage, feedrate = fast_rate, wait = True) # hovering over the material
-    dexarm.move_to(grab_x,grab_y, grab_signage, feedrate = fast_rate, wait = True) # grabbing the material
-    dexarm.move_to(grab_x,grab_y, grab_signage+100, feedrate = fast_rate, wait = True) # lift the material
-    dexarm.move_to(x_init, y_init, z_init, mode = "G0", feedrate = 3000) 
-    print("$") # this is meant to open the door
+    sensor_vl53l4cd_sat.VL53L4CD_StopRanging(); // Stop ranging
 
-# once the door opens the turn table will move to it's 0 angle
+    // Wait for a bit to spread the measurements over 1 second
+    delay(25); // This delay may need adjustment for precise timing
+  }
 
-# placing the material down
-def placedown_material(dexarm):
-    # will want to have the angles close to home but not really
-    # there are multiple angles for smooth transition as the dexarm will use the fastest path to get to the position
-    # dexarm.move_to(-200, 0, 30, None, feedrate = slow_rate, wait = True)
-    # dexarm.move_to(-210, 95, 30, None, feedrate = slow_rate, wait = True)
+  if (validMeasurements > 0) {
+    float averageDistance = totalDistance / validMeasurements;
+    Serial.print("$M");
+    Serial.println(averageDistance);
+  } else {
+    Serial.println("Measurement Error");
+  }
 
-    dexarm.move_to(-165, 165, 40, None, feedrate = fast_rate, wait = True)
-    # dexarm.move_to(-140, 185, 40, None, feedrate= fast_rate, wait = True)
-    rotate_first(dexarm)
-    time.sleep(3)
+  Serial.println("$D"); // Indicate to the PC that the measurements are complete
+}
 
-    dexarm.move_to(0, 235, 15, None, feedrate = fast_rate, wait = True) # dexarm in front of the laser cutter
-    dexarm.move_to(0, 235, 0, None, feedrate = fast_rate, wait = True)
+void loop() {
+  if (Serial.available()) {
+    String command = Serial.readStringUntil('\n');
+    Serial.print("given command: ");
+    Serial.println(command);
 
-    # the dexarm going inside of the laser cutter after this
-    dexarm.move_to(-30, 335, -5, None, feedrate = fast_rate, wait = True)    
-    dexarm.move_to(-30, 335, -70, None, feedrate = fast_rate, wait = True) 
-    dexarm.move_to(-30, 335, - 100, None, feedrate = fast_rate, wait = True)
-    dexarm.air_picker_place()
-    dexarm.air_picker_stop()
-    time.sleep(2)
-    dexarm.move_to(-30, 335, - 80, None, feedrate = fast_rate, wait = True)
-
-    # moving the material to a better position
-    correct = 0
-    for correct in range(2):
-        x_corr = [5, -20, 0, 20, -40]
-        y_corr = [330, 350, 360, 370]
-        z_corr = [-90, -100, -110, -113, -114]
-
-        dexarm.move_to(x_corr[0], y_corr[0], z_corr[1])
-        dexarm.move_to(x_corr[0], y_corr[0], z_corr[4])
-        dexarm.move_to(x_corr[0], y_corr[1], z_corr[4])
-        dexarm.move_to(x_corr[0], y_corr[1], z_corr[1])
-
-        dexarm.move_to(x_corr[1]-10, y_corr[1]-15, z_corr[1])
-        dexarm.move_to(x_corr[1]-10, y_corr[1]-15, z_corr[4])
-        dexarm.move_to(x_corr[1]-10, y_corr[3]-15, z_corr[4])
-        dexarm.move_to(x_corr[1]-10, y_corr[3]-15, z_corr[1])
-
-        dexarm.move_to(x_corr[2], y_corr[2], z_corr[0])
-        dexarm.move_to(x_corr[2], y_corr[2], z_corr[3])
-        dexarm.move_to(x_corr[4], y_corr[2], z_corr[3])
-        dexarm.move_to(x_corr[4], y_corr[2], z_corr[1])
-
-        dexarm.move_to(x_corr[3]-10, y_corr[0]+5, z_corr[0])
-        dexarm.move_to(x_corr[3]-10, y_corr[0]+5, z_corr[2])
-        dexarm.move_to(x_corr[4], y_corr[0]+5, z_corr[2])
-        dexarm.move_to(x_corr[4], y_corr[0]+5, z_corr[1])
-        
-
-    # getting the arm outside of the laser cutter
-    dexarm.move_to(0, 360, -70, None, feedrate = fast_rate, wait = True) 
-    dexarm.move_to(0, 360, 0, None, feedrate = fast_rate, wait = True)
-    dexarm.move_to(0, 235, 7, None, feedrate = fast_rate, wait = True)
-    # dexarm.move_to(-140, 185, 40, None, feedrate= fast_rate, wait = True)
-    # dexarm.move_to(-210, 95, 30, None, feedrate = slow_rate, wait = True)
-    dexarm.move_to(-165, 165, 20, None, feedrate = fast_rate, wait = True)
-    dexarm.move_to(-240, 0, 20, None, feedrate = fast_rate, wait = True)
-
-    dexarm.rotate_init()
-
-    # dexarm will go back to it's place so that it won't be in the way of the door
-    print("$") # this is meant to close the door after
-
-# the laser cutter should run with the implemented gcode
-# door will open again
-
-# pick up from laser cutter
-def laser_pick_up(dexarm):
-    
-    pick_x = -60
-    pick_y = 340
-
-    dexarm.move_to(-240, 0, 30, None, feedrate = fast_rate, wait = True)
-    # dexarm.move_to(-210, 95, 30, None, feedrate = slow_rate, wait = True)
-
-    dexarm.move_to(-165, 165, 20, None, feedrate = fast_rate, wait = True)
-    # dexarm.move_to(-140, 185, 40, None, feedrate= fast_rate, wait = True)
-    dexarm.move_to(0, 235, 20, None, feedrate = fast_rate, wait = True) # dexarm in front of the laser cutter
-
-    # the dexarm going inside of the laser cutter after this
-    dexarm.move_to(pick_x, pick_y, 20, None, feedrate = fast_rate, wait = True)    
-    dexarm.move_to(pick_x, pick_y, -70, None, feedrate = fast_rate, wait = True) 
-    dexarm.move_to(pick_x, pick_y, - 110, None, feedrate = fast_rate, wait = True)
-    dexarm.air_picker_pick()
-    dexarm.move_to(pick_x, pick_y, - 110, None, feedrate = fast_rate, wait = True)
-
-
-    # getting the arm outside of the laser cutter
-    dexarm.move_to(pick_x, pick_y, -70, None, feedrate = fast_rate, wait = True) 
-    # dexarm.move_to(pick_x, pick_y, 0, None, feedrate = fast_rate, wait = True)
-    dexarm.move_to(pick_x, pick_y, 30, None, feedrate = fast_rate, wait = True) 
-
-    dexarm.move_to(pick_x, 235, 30, None, feedrate = fast_rate, wait = True)
-    # dexarm.move_to(-140, 185, 40, None, feedrate= fast_rate, wait = True)
-    # dexarm.move_to(-210, 95, 30, None, feedrate = slow_rate, wait = True)
-    dexarm.move_to(-165, 165, 30, None, feedrate = fast_rate, wait = True)
-    dexarm.move_to(-235, 0, 35, None, feedrate = fast_rate, wait = True)
-    print("$") # this is the command so that the turn table will move 180 degrees
-
-
-def placeforqc(dexarm):
-    dexarm.move_to(-235, 0, 30, None, feedrate = fast_rate, wait = True)
-    dexarm.move_to(-330, 0, 30, None, feedrate = fast_rate, wait = True)
-    dexarm.move_to(-330, 0, -50, None, feedrate = fast_rate, wait = True)
-    dexarm.air_picker_place()
-    dexarm.move_to(-330, 0, 30, None, feedrate = fast_rate, wait = True)
-    dexarm.move_to(-235, 0, 30, None, feedrate = fast_rate, wait = True)
-
-
-# the conveyor belt moving for a while
-def conveyor(dexarm):
-    dexarm.conveyor_belt_move(2000, 15000)
-
-# the placement for passing the name tag over
-def place_nametag(dexarm):
-    y_zero = 0
-    z_maxheight = 50
-    z_zero = 0
-    z_placement = -80
-    dexarm.move_to(-245, y_zero, z_maxheight, feedrate = fast_rate)
-    dexarm.move_to(-380, y_zero, z_maxheight, feedrate = fast_rate)
-    dexarm.move_to(-380, y_zero, z_zero, feedrate = fast_rate)
-    dexarm.rotate_to_position(45)
-    dexarm.move_to(-380, y_zero, z_placement, feedrate = fast_rate)
-    dexarm.air_picker_place()
-    dexarm.air_picker_stop()
-    dexarm.move_to(-380, y_zero, z_placement, feedrate = fast_rate)
-    dexarm.move_to(-380, y_zero, z_zero, feedrate = fast_rate)
-    dexarm.move_to(-380, y_zero, z_maxheight, feedrate = fast_rate)
-    dexarm.move_to(-245, y_zero, z_maxheight, feedrate = fast_rate)
+    if (command == "$M") {
+      distSensor();
+    }else if(command == "$H"){ //home position
+      init_home();
+      Serial.println("moved to Home Position");
+    }else if(command == "$I"){ //init position -> used for dexarm to have room to init
+      move(position_init);
+      Serial.println("pre init for dexarm");
+    }else if(command == "$P1"){ //position 1 for picking up material
+      move(position1);
+      Serial.println("moved  to position 1");
+    }else if(command == "$P2"){ //position 2 for dropping material on conveyer belt
+      move(position2);
+      Serial.println("moved to position 2");
+    }else{
+      Serial.println("invalid input");
+    }
+  }
+}
